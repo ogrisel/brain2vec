9a624afe5c2d2184d57671b8a19c603c94096ad0
Olivier Grisel <olivier.grisel@ensta.org>
2017-05-02 22:31:05 +0200

    Convergence tests on random labels for the model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from keras.models import Model
 from keras.layers import Input, Dense, Dropout, merge
+from keras import optimizers
 import keras.backend as K
 
 
@@ -35,12 +36,31 @@ def make_models(input_dim, embedding_size=32, embedding_bias=False,
 if __name__ == "__main__":
     import numpy as np
     rng = np.random.RandomState(42)
-    n_samples = 100
+    n_samples = 200
     n_features = int(1e5)
     a = rng.randn(n_samples, n_features)
     b = rng.randn(n_samples, n_features)
-    y = rng.randint(low=0, high=2, size=n_samples)
 
-    embedding_model, siamese_model = make_models(n_features)
-    print(embedding_model.predict(a).shape)
-    print(siamese_model.predict([a, b]).shape)
+    embedding_size = 32
+    embedding_model, siamese_model = make_models(
+        n_features, embedding_size=embedding_size)
+
+    # Smoke tests: check the output shape of predictions of randomly
+    # initialized models
+    assert embedding_model.predict(a).shape == (n_samples, embedding_size)
+    assert siamese_model.predict([a, b]).shape == (n_samples, 1)
+
+    # Check that we can fit balanced random labels
+    y = rng.randint(low=0, high=2, size=n_samples)
+    optimizer = optimizers.SGD(lr=0.001, momentum=0.9, nesterov=True)
+    # optimizer = optimizers.Adam(lr=0.00001)
+    siamese_model.compile(optimizer=optimizer, loss='binary_crossentropy',
+                          metrics=['accuracy'])
+    trace = siamese_model.fit([a, b], y, validation_split=0.2, epochs=30)
+
+    # The model should be able to overfit the training data
+    assert trace.history['loss'][-1] < 1e-4
+    assert trace.history['acc'][-1] >= 0.95
+
+    # The test accuracy should be random
+    assert 0.35 <= trace.history['val_acc'][-1] <= 0.65
