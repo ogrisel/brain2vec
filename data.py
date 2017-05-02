3d56e1d78082122867b490697ea80da0a1f5ee69
Alexandre Abraham <a.abraham@criteo.com>
2017-05-02 19:52:32 +0200

    Fix return type

--- data.py
+++ data.py
@@ -27,32 +27,38 @@ def generate_learning_set(array, random_permutation=True, offset=0):
 
     Returns
     -------
-    learning_set: list of (img_a, img_b, label)
-        Two consecutive images, label is 1 if images are ordered, 0 otherwise
+    learning_set: (list of img, list of img, list of label)
+        Lists of consecutive indices and list of labels. If label is 0, indices are ordered decreasingly
     """
     np.random.seed()
 
-    learning_set = []
+    ia_list = []
+    ib_list = []
+    label_list = []
     for (ia, ib) in _consecutive_index_generator(array.shape[0], offset=offset):
-        label = np.random.randint(0, 2)
-        if label == 0:
-            ia, ib = ib, ia
-        learning_set.append((ia, ib, label))
+        label = 1
+        if random_permutation:
+            label = np.random.randint(0, 2)
+            if label == 0:
+                ia, ib = ib, ia
+        ia_list.append(ia)
+        ib_list.append(ib)
+        label_list.append(label)
 
-    return learning_set
+    return ia_list, ib_list, label_list
 
 
 if __name__ == '__main__':
     array = np.arange(5)
     res = generate_learning_set(array)
-    assert(len(res) == 4)
-    for ia, ib, label in res:
+    assert(len(res[0]) == 4)
+    for ia, ib, label in zip(*res):
         if label == 0:
             ia, ib = ib, ia
-
-    res = generate_learning_set(array, random_permutation=False, offset=1)
-    assert(len(res) == 3)
-    for ia, ib, label in res:
         assert(ia < ib)
+    res = generate_learning_set(array, random_permutation=False, offset=1)
+    assert(len(res[0]) == 3)
+    for ia, ib, label in zip(*res):
         assert(label == 1)
+        assert(ia < ib)
     print('Basic testing is OK')
