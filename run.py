01f90219473e9c02252daab2e1a797ae61d1008d
Olivier Grisel <olivier.grisel@ensta.org>
2017-05-04 12:15:03 +0200

    wip load more subject data

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import os
+import os.path as op
 import data
 import model
 import numpy as np
@@ -8,11 +8,23 @@ from joblib import Memory
 
 
 data_root_path = './hcp_olivier'
-subject_id = '102816'
+# subject_ids = ['102816', '102311']
+subject_ids = ['102816']
+session_ids = [1, 2]
+# session_ids = [1]
 
-# Localize data through file system relative indexing method
-path = os.path.join(data_root_path, subject_id, 'MNINonLinear', 'Results',
-                    'rfMRI_REST1_LR', 'rfMRI_REST1_LR.npy')
+
+def get_paths(data_root_path, subject_ids, session_ids=[1, 2]):
+    all_paths = []
+    for subject_id in subject_ids:
+        for session_id in session_ids:
+            path = op.join(data_root_path, subject_id,
+                           'MNINonLinear', 'Results',
+                           'rfMRI_REST%d_LR' % session_id,
+                           'rfMRI_REST%d_LR.npy' % session_id)
+            all_paths.append(path)
+    return all_paths
+
 
 m = Memory(cachedir='/tmp')
 
@@ -24,7 +36,10 @@ def pca_transform(x, n_components=128, seed=0):
 
 
 # Use data loading library to load data
-# x = np.load(path)
+all_paths = get_paths(data_root_path, subject_ids, session_ids=session_ids)
+x = np.vstack([np.load(path).astype(np.float32) for path in all_paths])
+print("original data shape:")
+print(x.shape)
 # print("extracting pca components:")
 # x = pca_transform(x)
 # a, b, y = data.generate_learning_set(x, scans_to_average=2)
@@ -39,18 +54,20 @@ def make_synthetic_data(n_samples, n_signal_features, n_noise_features,
     return np.hstack([signal_features, noise_features])
 
 
-x = make_synthetic_data(1000, 100, 0)
+# x = make_synthetic_data(1000, 100, 0)
 # a, b, y = data.generate_learning_set(x)
 # input_dim = a.shape[1]
 window_size = 5
-scans, labels = data.generate_permutations(x, scans_to_permute=window_size)
+scans, labels = data.generate_permutations(x, scans_to_permute=window_size,
+                                           backward_is_ok=True)
+del x
 input_dim = len(scans[0][0])
 
 # Generate the model
 embedding_model, siamese_model = model.make_permutation_models(
     input_dim, n_inputs=window_size)
 
-# optimizer = optimizers.SGD(lr=0.001, momentum=0.9, nesterov=True)
+# optimizer = optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True)
 optimizer = optimizers.Adam(lr=0.001)
 
 siamese_model.compile(optimizer=optimizer, loss='binary_crossentropy',
