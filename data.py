67a3507ac90b25e632673a4f928a90bdd9e8cb3a
Alexandre Abraham <a.abraham@criteo.com>
2017-05-04 10:46:14 +0200

    Add permutations generator

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 
 def _consecutive_index_generator(length, offset=1):
@@ -53,6 +54,69 @@ def generate_learning_set(array, scans_to_average=1, random_permutation=True, of
     return np.asarray(ia_list), np.asarray(ib_list), np.asarray(label_list)
 
 
+def generate_permutations(array, scans_to_permute, balanced_wrt_ordered=True, backward_is_ok=False, seed=0):
+
+    assert(scans_to_permute > 1)  # Not pertinent without 1 or less scans
+
+    rng = np.random.RandomState(seed)
+
+    n_samples = array.shape[0]
+
+    scans_list = [list() for i in range(scans_to_permute)]
+    labels_list = []
+
+    for (ia, ib) in _consecutive_index_generator(n_samples, offset=scans_to_permute):
+        order = np.arange(scans_to_permute)
+        if balanced_wrt_ordered:
+            # Flip a coin, ordered or not
+            label = rng.randint(0, 2)
+            if label == 0:
+                while (order == np.arange(scans_to_permute)).all():
+                    rng.shuffle(order)
+        else:
+            # Normal order has equal probability than all other solutions
+            rng.shuffle(order)
+            label = int((order == np.arange(scans_to_permute)).all())
+        
+        if backward_is_ok and (order == np.arange(scans_to_permute)[::-1]).all():
+            label = 1
+
+        for i, ic in enumerate(order):
+            scans_list[i].append(array[ic + ia])
+        labels_list.append(label)
+
+    return scans_list, labels_list
+
+
+def one_hot_permutation(array, scans_to_permute, balanced_wrt_ordered=True, seed=0):
+
+    assert(scans_to_permute > 1)  # Not pertinent without 1 or less scans
+
+    rng = np.random.RandomState(seed)
+
+    n_samples = array.shape[0]
+
+    # Generate an index of all possible permutations
+    permutations = itertools.permutations(range(scans_to_permute))
+
+    # Prepare the label matrix
+    labels = np.zeros(n_samples - scans_to_permute - 1, len(permutations))
+
+    ia_list = []
+    ib_list = []
+
+    for (ia, ib) in _consecutive_index_generator(n_samples, offset=offset + scans_to_permute):
+        order = np.arange(scans_to_permute)
+        if balanced_wrt_ordered:
+            # Flip a coin, ordered or not
+            label = rng.randint(0, 2)
+            if label == 0:
+                rng.shuffle(order)
+        else:
+            pass
+    pass
+
+
 if __name__ == '__main__':
     array = np.arange(5)
     res = generate_learning_set(array)
@@ -69,6 +133,13 @@ if __name__ == '__main__':
         assert(ia < ib)
 
     res = generate_learning_set(array, scans_to_average=2, random_permutation=False, offset=1)    
+    assert(len(res[0]) == 3)
+    for ia, ib, label in zip(*res):
+        assert(label == 1)
+        assert(ia < ib)
+
+    scans, labels = generate_permutations(array, scans_to_permute=3)
+    print(scans, labels)    
     assert(len(res[0]) == 3)
     for ia, ib, label in zip(*res):
         assert(label == 1)
