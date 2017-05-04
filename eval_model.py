f7a4483db900a974b3da316721e999fe5d63d649
Olivier Grisel <olivier.grisel@ensta.org>
2017-05-04 14:39:31 +0200

    plotting and model evaluation

---
+++ eval_model.py
@@ -0,0 +1,39 @@
+import os.path as op
+import data
+import numpy as np
+from keras.models import load_model
+
+
+data_root_path = './hcp_olivier'
+subject_ids = ['103414']
+session_ids = [1]
+models_folder = 'checkpoints'
+
+
+def get_paths(data_root_path, subject_ids, session_ids=[1, 2]):
+    all_paths = []
+    for subject_id in subject_ids:
+        for session_id in session_ids:
+            path = op.join(data_root_path, subject_id,
+                           'MNINonLinear', 'Results',
+                           'rfMRI_REST%d_LR' % session_id,
+                           'rfMRI_REST%d_LR.npy' % session_id)
+            all_paths.append(path)
+    return all_paths
+
+
+all_paths = get_paths(data_root_path, subject_ids, session_ids=session_ids)
+x = np.vstack([np.load(path).astype(np.float32) for path in all_paths])
+print("original data shape:")
+print(x.shape)
+window_size = 5
+scans, labels = data.generate_permutations(x, scans_to_permute=window_size,
+                                           backward_is_ok=True)
+del x
+input_dim = len(scans[0][0])
+
+print('loading model:')
+model = load_model('/tmp/model.036-0.82.h5')
+print('computing predictions on new data')
+predicted = model.predict(scans)
+print("accuracy: %0.3f" % np.mean((predicted > 0.5) == labels))
--- plot_embeddings.py
+++ plot_embeddings.py
@@ -4,22 +4,32 @@ from nilearn.input_data import NiftiMasker
 from nilearn.image import index_img
 from data import generate_learning_set
 import os
+import os.path as op
 import numpy as np
 import nibabel
+from keras.models import load_model
 
 
 data_root_path = './hcp_olivier'
 subject_id = '102816'
+folder = 'visualizations'
+
+weights = load_model('checkpoints/model.000-0.51.h5').get_weights()[0]
+images = weights.T
 
 # Localize data through file system relative indexing method
-path = os.path.join(data_root_path, subject_id, 'MNINonLinear', 'Results', 'rfMRI_REST1_LR', 'rfMRI_REST1_LR.npy')
+# path = os.path.join(data_root_path, subject_id, 'MNINonLinear', 'Results', 'rfMRI_REST1_LR', 'rfMRI_REST1_LR.npy')
+# images = np.load(path)[:32]
 
 # Create a masker to unmask data
 masker = NiftiMasker(mask_img=os.path.join(data_root_path, 'mask_img.nii.gz'))
 masker.fit()
 
-atlas = masker.inverse_transform(np.load(path)[:32])
+atlas = masker.inverse_transform(images)
 
+if not op.exists(folder):
+    os.makedirs(folder)
 # Plot 32 brains
 for i in range(32):
-    plotting.plot_stat_map(index_img(atlas, i), output_file='brain_{}.png'.format(i))
+    plotting.plot_stat_map(index_img(atlas, i),
+                           output_file=folder + '/brain_{:02d}.png'.format(i))
